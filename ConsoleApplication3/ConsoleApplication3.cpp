f4be086af0a72ad046c4e86cd2b99bd1038843c9
rcanelas <50590195+rcanelas@users.noreply.github.com>
2025-06-05 19:37:19 +0100

    Update ConsoleApplication3.cpp
    teste

--- ConsoleApplication3/ConsoleApplication3.cpp
+++ ConsoleApplication3/ConsoleApplication3.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World! Ruben  \n";
+    std::cout << "Hello World! Ruben  \n"; coisas
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
